c2367c1b485635443b2e64207ee9db00e81e30d0
rp729 <rp729@yahoo.com>
2020-01-10 14:26:09 -0500

    updates

--- class_pract/perf_lab_2.c
+++ class_pract/perf_lab_2.c
@@ -8,7 +8,7 @@ int main(void)
 
     //Prompt user for input
     printf("Enter a number to shift :");
-    fscanf(stdin,"%hhd",&user_input);
+    fscanf(stdin,"%d",&user_input);
     
     //Display final result
     fprintf(stdout,"\nShifting to the right\nNumber >> 5 = %d\n",user_input>>5);
