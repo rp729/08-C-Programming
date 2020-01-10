4797ef71cd33addcbf64d5327bee5caa31be7a4a
rp729 <rp729@yahoo.com>
2020-01-10 14:00:48 -0500

    shit bits

---
+++ class_pract/perf_lab_10.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+//gcc file.c -lm (links math header)
+
+/*
+#define SCNu32 "lu"
+decimal scanf format for uint32_t
+*/
+
+int main(void)
+{
+    // unsigned character
+    unsigned char user_input;
+    unsigned char a = 1;
+
+    printf("Enter a number to shift :");
+    fscanf(stdin,"%hhd",&user_input);
+
+    if (user_input & (1<<a))
+    {
+        fprintf(stdout,"\nShifting to the right\nNumber << 1 = %d\n",user_input>>1);
+    }
+
+    else
+    {
+        printf("First bit is not shifted to the left.\n");
+    }
+
+    return 0;
+}
