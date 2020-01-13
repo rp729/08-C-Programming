b0b81fb30b47e662f588cfd5e2b1326f4d3d7355
rp729 <rp729@yahoo.com>
2020-01-13 11:44:53 -0500

    Performance Lab 12

---
+++ class_pract/perf_lab_12.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int index_i(int i)
+{
+    if (i == 0)
+    {
+        return 3;
+    }
+    
+    else
+    {
+        return 1 + (i >> 31) - (-1 >> 31);
+    }
+}
+
+void check(int n)
+{
+    int val = index_i(n);
+    //printf("Value :%d\n",val);
+
+    if (val == 1)
+    {
+        printf("negative\n");
+    }
+
+    else if (val == 3)
+    {
+        printf("zero\n");
+    }
+    
+    else
+    {
+        printf("positive\n");
+    }
+    
+    
+
+}
+
+int main(void)
+{
+    int user_input = 0;
+    printf("Enter a number :");
+    scanf("%d",&user_input);
+
+    check(user_input);
+
+    return 0;
+}
